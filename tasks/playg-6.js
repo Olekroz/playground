72a6c4aefe8b449428a3a6f3f5d30114f4af0354
Anna <anna.wisniowska@onwelo.com>
2023-01-23 18:55:40 +0100

    6th task completed

---
+++ tasks/playg-6.js
@@ -0,0 +1,11 @@
+// Use the reduce() method to count the number of occurrences of a specific element in an array.
+
+const arr = [3, 19, 44, 19, 50, 63, 63, 19, 72, 83, 19, 313, 3, 313, 19, 313, 19, 50];
+
+const count = (arr, value) => {
+  return arr.reduce((acc, el) => {
+    return value === el ? acc + 1 : acc;
+  }, 0);
+};
+
+console.log(count(arr, 19));

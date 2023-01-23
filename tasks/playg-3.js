c6265b9ae2d934be957787f76fb9c7a404571e43
Anna <anna.wisniowska@onwelo.com>
2023-01-23 15:34:59 +0100

    3rd task completed

---
+++ tasks/playg-3.js
@@ -0,0 +1,8 @@
+// Use the map() method to square each element of an array of numbers and assign the returned value to a new variable.
+
+arr = [2, 3, 5, 10, 12, 27];
+
+const square = arr.map((el) => {
+  const squared = Math.pow(el, 2);
+  return squared;
+});

b5047f33c60f152df50b605322f5a7983868c5fc
Anna <anna.wisniowska@onwelo.com>
2023-01-23 15:42:50 +0100

    5th task done

---
+++ tasks/playg-5.js
@@ -0,0 +1,5 @@
+// Use the sort() method to sort an array of strings alphabetically in ascending order.
+
+const arr = ["dog", "bird", "elephant", "cat", "unicorn", "dragon", "hippogriff"];
+
+const sorted = arr.sort();

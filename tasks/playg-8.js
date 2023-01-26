268e63554966e4d75e4c41e018f75c36e1d69bd4
Anna <anna.wisniowska@onwelo.com>
2023-01-26 16:23:40 +0100

    playg-8 completed

---
+++ tasks/playg-8.js
@@ -0,0 +1,7 @@
+// Use the slice() method to create a new array with a subset(first half of array) of elements from an existing array
+
+const arr = ["dog", "bird", "elephant", "cat", "unicorn", "dragon", "hippogriff"];
+
+const half = arr.slice(0, 4);
+
+console.log(half);
